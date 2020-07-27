4d1170052017d637804c6a4f0ac414cb88dac522
Rucha Dave <ruchadave16@gmail.com>
2020-07-26 20:01:49 -0400

    Commented code

--- data_collected/combine_files.py
+++ data_collected/combine_files.py
@@ -1,14 +1,19 @@
+######################################################################################################################################################################
+#This code cleans up the files and combines the individual ones into one file, in the directory combined_files
+######################################################################################################################################################################
 #Import all modules needed to create files
 import os 
 import glob
 import pandas as pd 
 
 #Move to right path
+os.chdir("data_collected")
 os.chdir("individual_data_csv")
 
 #List all CSV Files in directory
 rootdir = 'C:/Users/kinnari/Documents/GitHub/piPACT-data-analysis/data_collected/individual_data_csv/**/*'
 
+#Create list with all file names that start with the beginning of the scenario
 none_list = glob.glob('none*.csv')
 rec_thin_list = glob.glob('rec_thin*.csv')
 trans_thin_list = glob.glob('trans_thin*.csv')
@@ -23,11 +28,9 @@ both_hand_list = glob.glob('both_hand*.csv')
 all_scenarios = [none_list, rec_thin_list, trans_thin_list, both_thin_list, rec_purse_list, trans_purse_list, both_purse_list, rec_hand_list, trans_hand_list, both_hand_list]
 all_scenarios_names = ["none_list", "rec_thin_list", "trans_thin_list", "both_thin_list", "rec_purse_list", "trans_purse_list", "both_purse_list", "rec_hand_list", "trans_hand_list", "both_hand_list"]
 
-#Concatenate files that belong to one scenario:
-def createOneCSV(file_list, output_file):
-	result = pd.concat([pd.read_csv(file) for file in file_list])
-	result.to_csv(output_file, index=False, encoding="utf-8")
-
-for i in range(len(all_scenarios_names)):
-	output_file = all_scenarios_names[i] + ".csv"
-	createOneCSV(all_scenarios[i], output_file)
+#Concatenate files that belong to one scenario into one file:
+i = 0
+for name in all_scenarios:
+	combined_file = pd.concat([pd.read_csv(f) for f in name])
+	combined_file.to_csv(all_scenarios_names[i] + ".csv", index = False, encoding = 'utf-8-sig')
+	i += 1
